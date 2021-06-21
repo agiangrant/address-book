8d8238ac88d91e7ff2a431df5add756f10cbb235
Andrew Giangrant <andrew.giangrant@outlook.com>
2021-06-20 22:21:04 -0700

    routing entries to list and detail components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactDetailComponent } from 'src/contacts/components/contact-detail/contact-detail.component';
+import { ContactListComponent } from 'src/contacts/components/contact-list/contact-list.component';
+import { ContactsModule } from 'src/contacts/contacts.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contacts',
+    component: ContactListComponent,
+  },
+  { path: 'contacts/:contact_id', component: ContactDetailComponent },
+  { path: '', redirectTo: '/contacts', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), ContactsModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
